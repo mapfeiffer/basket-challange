93a510992ff2e0f6c32ea0de76563f5d954de8c2
Martin Pfeiffer <info@getgnu.de>
2025-08-28 12:59:14 +0200

    fix missing timestamps on baskets and remove secret from env.dev and remove unnecessary basketItem repository

--- src/Entity/BasketItem.php
+++ src/Entity/BasketItem.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use App\Repository\BasketItemRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 #[ORM\Entity(repositoryClass: BasketItemRepository::class)]
 class BasketItem
 {
+    use TimestampableEntity;
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
